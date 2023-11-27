acc4d748c527cad7b93ab569e033fe05a32dd8d9
zasalast <zas_91@hotmail.com>
2023-11-26 20:18:57 -0500

    update programs

--- Program.cs
+++ Program.cs
@@ -1,8 +1,16 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Agenda_mvc__ADO.NET_MySQL_ASP.NET_CORE_6._0.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddSingleton(new Contexto(builder.Configuration.GetConnectionString("Conexion")));
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(o =>
+    {
+        o.LoginPath = "/Persona/Login";
+    });
+builder.Services.AddHttpContextAccessor();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -14,6 +22,7 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
